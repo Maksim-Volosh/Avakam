3387188c934366233703ad780a0076a270be9a0e
Vlad Lesun <vladlesunmma@gmail.com>
2024-11-25 17:54:13 +0300

    feat: add popup sort in index.html listing_found.html

--- static/js/index.js
+++ static/js/index.js
@@ -19,4 +19,24 @@ const adSwiperMainOptions = {
 
 const swiperAdMainPage = new Swiper(adSwiperMainPageNode, adSwiperMainOptions);
 
-//! path listing_not_found.html
+//! popups
+const sortProductNode = document.getElementById('js-products-sort'),
+	popupSortProductNode = document.getElementById('js-popup-sort'),
+	popupItemSortProductNode = document.querySelectorAll('.popup__sort-btn');
+
+//! sort
+const handleSortPopupOpen = () => {
+	popupSortProductNode.classList.add('popup_open');
+};
+const handleSortPopupClose = () => {
+	popupSortProductNode.classList.remove('popup_open');
+};
+sortProductNode.addEventListener('click', event => {
+	event.stopPropagation();
+
+	popupSortProductNode.classList.add('popup_open');
+});
+window.addEventListener('click', handleSortPopupClose);
+popupItemSortProductNode.forEach(item => {
+	item.addEventListener('click', handleSortPopupClose);
+});
